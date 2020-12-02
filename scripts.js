bd59e16a7f396eaf8a25a4dd772875e1eecf7450
hvzy <hadimirza1@gmail.com>
2020-12-02 14:25:35 -0500

    Clean up code

--- scripts.js
+++ scripts.js
@@ -1,30 +1,28 @@
 /*----- app's state (variables) -----*/
 
-let clicks = 0
+let clicks = 0;
 
 /*----- cached element references -----*/
 
-let span1 = document.getElementById("span1")
-let span2 = document.getElementById("span2")
-let btn1 = document.getElementById("btn1")
-let btn2 = document.getElementById("btn2")
-
+let span1 = document.getElementById("span1");
+let span2 = document.getElementById("span2");
+let btn1 = document.getElementById("btn1");
+let btn2 = document.getElementById("btn2");
 
 /*----- event listeners -----*/
 
-btn1.addEventListener("click", countUp)
-btn2.addEventListener("click", init)
-
+btn1.addEventListener("click", countUp);
+btn2.addEventListener("click", init);
 
 /*----- functions -----*/
 
 function countUp() {
   // countdown()
   clicks += 1;
-  span1.innerHTML = clicks
+  span1.innerHTML = clicks;
 }
 
 function init() {
-  clicks = 0
-  span1.innerHTML = clicks
+  clicks = 0;
+  span1.innerHTML = clicks;
 }
